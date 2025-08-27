cba5d6347e9cdb64b7d5cff506a523a0bdc91b79
Quintessa <179593529+QuintessaCodes@users.noreply.github.com>
2025-08-27 16:02:34 +0100

    Added middleware

--- student_management/app/main.py
+++ student_management/app/main.py
@@ -1,4 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from middleware import log_requests
 
 app = FastAPI()
 
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.middleware("http")(log_requests)
