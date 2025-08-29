ff7821f4f9e8e20d79f39e8ab876dd19ee052492
Quintessa <179593529+QuintessaCodes@users.noreply.github.com>
2025-08-29 18:11:19 +0100

    Added middleware

--- notes_api/app/main.py
+++ notes_api/app/main.py
@@ -1,3 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from middleware import count_requests
 
 app = FastAPI()
+
+origins = ["http://localhost:3000", "http://127.0.0.1:5500"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.middleware("http")(count_requests)
