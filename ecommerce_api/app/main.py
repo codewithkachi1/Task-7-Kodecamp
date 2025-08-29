1402645a4f236cf2298051f597e4d5f87a4573cc
Quintessa <179593529+QuintessaCodes@users.noreply.github.com>
2025-08-29 01:11:47 +0100

    Added middleware

--- ecommerce_api/app/main.py
+++ ecommerce_api/app/main.py
@@ -1,4 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from middleware import measure_response_time
 
 app = FastAPI()
 
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.middleware("http")(measure_response_time)
